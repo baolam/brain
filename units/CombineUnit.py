4c5de2b76d3347583f9a419c7d64f88aa8dbec20
baolam <lam01662052827@gmail.com>
2024-03-12 22:12:33 +0700

    Updating

--- units/CombineUnit.py
+++ units/CombineUnit.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import Tuple, Dict
 
-from torch import Tensor, stack
+from torch import Tensor, stack, concatenate
 from . import Unit
 
 
@@ -13,25 +13,39 @@ class CombineUnit(Unit, ABC):
     def send(self, *args, **kwargs):
         pass
 
+
 class ConcatFeature(CombineUnit):
     def __init__(self, addr: Tuple[str | None], **kwargs):
         super().__init__(addr, **kwargs)
         self.__feature : Dict[str, Tensor] = {  }
+        self.__infor : Dict[str, str] = {  }
 
-    def recv(self, x: Tensor, idx : int = None, *args, **kwargs):
+    def recv(self, x: Tensor, idx : int = None, _from : str = None, *args, **kwargs):
         # Làm sao quyết định vị trí của đặc trưng?
         if idx is None:
             raise ValueError("Không có chỉ số khoảng!")
+        if _from is None:
+            raise ValueError("Đơn vị không tồn tại tên")
+        if not self.__feature.get(str(idx)) is None:
+            raise ValueError("Đã tồn tại!")
+        
         self.__feature[str(idx)] = x
+        self.__infor[str(idx)] = _from
 
     def forward(self, *args, **kwargs) -> Tensor:
-        pass
+        return self.feature()
     
     def feature(self, *args, **kwargs):
-        pass
+        output = []
+        for key in sorted(self.__feature.keys()):
+            output.append(self.__feature[key])
+        return concatenate(output)
 
     def clear_feature(self, *args, **kwargs):
         self.__feature.clear()
+
+    def infor_feature(self):
+        return self.__infor
 
 
 class MeanFeatureWithoutAdaptive(CombineUnit):
